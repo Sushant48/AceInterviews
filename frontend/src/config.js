9ab9e3a59059fa3f413004d70bfa1e2eea2e6114
Sushant <sushantsingh408sks@gmail.com>
2025-04-27 00:22:33 +0530

    url added

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,4 @@
-const BASE_URL = 'http://localhost:3000/api/v1';
+const BASE_URL = 'https://aceinterviews-1.onrender.com/api/v1';
+// const BASE_URL = 'http://localhost:3000/api/v1'; // For local development
 
 export default BASE_URL;
